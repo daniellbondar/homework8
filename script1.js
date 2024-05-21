da23a3f07e6187b7224cf92e38bb3f42549c8b9a
daniellbondar <165205277+daniellbondar@users.noreply.github.com>
2024-05-21 18:54:14 +0200

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,14 @@
+// let number = +prompt('Enter the number');
+// console.log(number ** 3);
+
+// let number1 = +prompt('Enter the number 1')
+// let number2 = +prompt('Enter the number 2');
+// console.log(number1 + number2);
+
+let userName = prompt('Enter your name');
+let userSurname = prompt('Enter your surname');
+let user_age = +prompt('Enter your age');
+let user_position = 2024 - user_age
+
+console.log(`Hello. My name is ${userName} ${userSurname}. I was born in ${user_position}. I'm ${user_age} years old.`);
+
